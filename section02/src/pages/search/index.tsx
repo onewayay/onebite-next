3ee1bd27336ec4bdd4969c8ce4a363ab7fffea39
onewayay <onewayay@naver.com>
2025-12-15 19:16:19 +0900

    17강

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,13 +1,21 @@
 import SearchableLayout from '@/components/searchable-layout';
 import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
 
 export default function Page() {
   const router = useRouter(); // 모든 라우터 정보가 다 들어있는 객체
 
   const { q } = router.query; // (localhost:3000/search?q=1) 에서 1
 
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
