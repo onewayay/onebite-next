6fb6b67e8f6719cc111186df37772ec01df9e5f3
onewayay <onewayay@naver.com>
2025-12-17 17:50:53 +0900

    21강~ 24강
    - SSG 1. 소개
    - SSG 2. 정적 경로에 적용하기
    - SSG 3. 동적 경로에 적용하기
    - SSG 4. 폴백옵션 설정하기

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,27 +1,27 @@
 import SearchableLayout from '@/components/searchable-layout';
 import { useRouter } from 'next/router';
-import { ReactNode } from 'react';
-import books from '@/mock/books.json';
+import { ReactNode, useEffect, useState } from 'react';
 import BookItem from '@/components/book-item';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
+import { BookData } from '@/type';
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
 
-  const books = await fetchBooks(q as string);
-
-  return { props: { books } };
-};
-
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter(); // 모든 라우터 정보가 다 들어있는 객체
+  const q = router.query.q; // (localhost:3000/search?q=1) 에서 1
 
-  const { q } = router.query; // (localhost:3000/search?q=1) 에서 1
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
+  };
+
+  useEffect(() => {
+    if (q) {
+      // 검색 결과를 불러오는 로직
+      fetchSearchResult();
+    }
+  }, [q]);
 
   return (
     <div>
