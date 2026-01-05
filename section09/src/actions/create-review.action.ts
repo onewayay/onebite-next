729b1b8f3941004a3f3995aede593d66a1cb47a4
onewayay <onewayay@naver.com>
2026-01-05 22:05:22 +0900

    63강

--- section09/src/actions/create-review.action.ts
+++ section09/src/actions/create-review.action.ts
@@ -1,6 +1,5 @@
 'use server';
-import { delay } from '@/util/delay';
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { revalidateTag } from 'next/cache';
 
 // 자동으로 이 함수는 서버 액션으로 설정된다.
 
@@ -17,8 +16,6 @@ export async function createReviewAction(_: any, formData: FormData) {
   }
 
   try {
-    await delay(2000);
-
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -34,18 +31,6 @@ export async function createReviewAction(_: any, formData: FormData) {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
-
-    // // 1. 두번재 인수 생략 :  특정 주소에 해당하는 페이지만 재검증
-    // revalidatePath(`/book/${bookId}`);
-
-    // // 2. 첫번째 인수로 폴더의 경로, 두번째 인수로는 'page' : 특정 경로의 모든 동적 페이지를 재검증
-    // revalidatePath(`/book/[id]`, 'page');
-
-    // // 3. 첫번째 인수로 재검증에 기준이 되는 레이아웃 파일이 위치한 경로,  두번째 인수로는 'layout' : 특정 레이아웃을 갖는 모든 페이지 재검증
-    // revalidatePath(`/(with-searchbar)`, 'layout');
-
-    // // 4. 첫번째 인수로 인덱스경로,  두번째 인수로는 'layout' : 모든 데이터를 재검증
-    // revalidatePath(`/`, 'layout');
 
     // 5. revalidateTag 사용 : 태그값을 기준으로 데이터 캐시 재검증
     revalidateTag(`review-${bookId}`);
--- section09/src/app/(with-searchbar)/page.tsx
+++ section09/src/app/(with-searchbar)/page.tsx
@@ -1,23 +1,9 @@
 import BookItem from '@/components/book-item';
 import style from './page.module.css';
 import { BookData } from '@/types';
-import { delay } from '@/util/delay';
-import { Suspense } from 'react';
-import BookListSkeleton from '@/components/skeleton/book-list-skeleton';
 import { Metadata } from 'next';
 
-// export const dynamic = 'force-dynamic';
-// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
-/*
-  1. auto : 기본값. 아무것도 강제하지 않음
-  2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
-  3. force-static : 페이지를 강제로 Static 페이지로 설정
-  4. error : 페이지를 강제로 Static 페이지로 설정 (설정하면 안되는 이유가 있다면 -> 빌드 오류)
-*/
-
 async function AllBooks() {
-  await delay(1500);
-
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: 'force-cache' }
@@ -37,8 +23,6 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
-  await delay(3000);
-
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -57,8 +41,6 @@ async function RecoBooks() {
   );
 }
 
-export const dynamic = 'force-dynamic';
-
 export const metadata: Metadata = {
   title: '한입 북스',
   description: '한입 북스에 등록된 도서를 만나보세요',
@@ -74,15 +56,11 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<BookListSkeleton count={3} />}>
-          <RecoBooks />
-        </Suspense>
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<BookListSkeleton count={10} />}>
-          <AllBooks />
-        </Suspense>
+        <AllBooks />
       </section>
     </div>
   );
